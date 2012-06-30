c647ba19298d973bb9a54105d01beea682bcf2cf
Pablo Novas <pjnovas@gmail.com>
2012-06-30 17:29:20 -0300

    missed file jeje

--- public/js/libs/jquery.slider.js
+++ public/js/libs/jquery.slider.js
@@ -61,7 +61,7 @@ var Slider = (function($) {
 	
 	var resize = function(){
 		var h100 = 700,
-			h = $(mainCtn).height(),
+			h = $(mainCtn).height(),d
 			w = $(mainCtn).width(),
 			mr = 0,
 			mt = (h * 0.05),
